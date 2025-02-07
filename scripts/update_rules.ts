205d3f3452337b2fa112111b72599b889ca53b06
littletao08 <cvghju@163.com>
2025-02-07 17:16:45 +0800

    Update update_rules.ts

--- scripts/update_rules.ts
+++ scripts/update_rules.ts
@@ -1,5 +1,5 @@
 const rulesets = [
-  ['https://raw.githubusercontent.com/littletao08/SiteRules/refs/heads/master/direct.list', 'DIRECT'],
+  ['https://raw.githubusercontent.com/littletao08/SiteRules/master/direct.list', 'DIRECT'],
   ['https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/LocalAreaNetwork.list', 'DIRECT'],
   ['https://raw.githubusercontent.com/zsokami/ACL4SSR/main/ChinaOnly.list', 'DIRECT'],
   ['https://raw.githubusercontent.com/zsokami/ACL4SSR/main/UnBan1.list', '🛩️ ‍墙内'],
